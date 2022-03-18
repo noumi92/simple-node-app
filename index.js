94b4d319451ac4b336b95cc53c4f3158e63d5df4
noumi92 <noumankarim1988@gmail.com>
2022-03-18 17:37:42 +0500

    simple node application

---
+++ index.js
@@ -0,0 +1,16 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    if(l<=0 || b<=0){
+        console.log(`(${l},${b}) <= Rectangle dimensions should be greater than zero`);
+    }else{
+        console.log(`(${l},${b}) Area = ${rect.area(l,b)}, Perimeter = ${rect.perimeter(l,b)}`)
+    }
+}
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,6);
+solveRect(-1,9);
